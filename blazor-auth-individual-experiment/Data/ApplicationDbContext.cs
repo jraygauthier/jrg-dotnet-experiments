58fdbc2ad2c3faf45772c7b25c14fe1b266e004a
Raymond Gauthier <jraygauthier@gmail.com>
2020-10-15 16:31:07 -0400

    Add blazor auth individual experiment

---
+++ blazor-auth-individual-experiment/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazor_auth_individual_experiment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
